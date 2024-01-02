0efbbfe3f4e46c459068cdb78afb8ba860ab5342
LukasLapienis <lapienislukasp@gmail.com>
2024-01-02 18:17:43 +0200

    favorites feature added

---
+++ modules/favorites.js
@@ -0,0 +1,52 @@
+// funkcija sukurti favorites 
+
+export function addFavorites(className) {
+
+    let cards = Array.from(document.getElementsByClassName(className))
+    let dishNumber = 1
+    cards.forEach(card => {
+        const heart = document.createElement("i")
+        heart.setAttribute("class", "fa fa-heart")
+        heart.setAttribute("id", `dish${dishNumber}`)
+        heart.style.position = "absolute"
+        heart.style.top = "3%"
+        heart.style.right = "3%"
+        heart.style.fontSize = "40px"
+
+        card.append(heart)
+        dishNumber ++
+    })
+
+    let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
+    const redHeart = document.querySelectorAll("i") 
+    const sortByNumber = (a, b) => (Number(a.match(/(\d+)/g)[0]) - Number(b.match(/(\d+)/g)[0]))  
+
+    redHeart.forEach((heart) => {
+        const imageId = heart.getAttribute("id")
+        if (favorites.includes(imageId)) {
+            heart.style.color = "red"
+            heart.style.opacity = "90%"
+        } else {
+            heart.style.color = "white"
+            heart.style.opacity = "30%"
+        }
+        heart.addEventListener("click", (event) => {
+            event.preventDefault()
+            if (favorites.includes(imageId)) {
+                heart.style.color = "white"
+                heart.style.opacity = "30%"
+                favorites = favorites.filter(id => id !== imageId);
+            } else {
+                heart.style.color = "red"
+                heart.style.opacity = "90%"
+                favorites.push(imageId);
+            }
+            if (favorites.length === 0) {
+                localStorage.removeItem("favorites")
+            } else {
+                localStorage.setItem('favorites', JSON.stringify(favorites.sort(sortByNumber)));
+            }
+        })
+    })
+}
--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 import { data } from "./data.js";
+import { addFavorites } from "./modules/favorites.js"
 console.log(data);
+
+
+addFavorites("col-md-6")
