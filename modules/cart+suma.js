cdb8ea533dcf71862ae5914a1e8f6f55b5192bcb
Monika <okonecnikovaite.monika@gmail.com>
2024-01-04 21:19:10 +0200

    feat/prie cart bendras uzsakymu skaicius

--- modules/cart+suma.js
+++ modules/cart+suma.js
@@ -1,10 +1,40 @@
 const kainaNavSelect = document.querySelector(".navbar-nav");
-const kainaNav = document.createElement("p");
+const kainaNav = document.createElement("a");
 kainaNavSelect.append(kainaNav);
-kainaNav.innerText= "Krepšelio suma";
+
+kainaNav.setAttribute("href","cart.html")
+//style
 kainaNav.style.color = "black";
-kainaNav.style.fontSize = "2.5em";
+kainaNav.style.fontSize = "2.5rem";
 kainaNav.style.marginLeft = "20px";
 kainaNav.style.fontWeight = "500";
 kainaNav.style.fontFamily = "'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif";
 kainaNav.style.fontStyle = "italic";
+kainaNav.style.textDecoration = "none"
+//hover effect
+kainaNav.addEventListener('mouseover',() =>{
+    kainaNav.style.color = 'palevioletred';
+});
+kainaNav.addEventListener('mouseout',()=>{
+    kainaNav.style.color = 'black'
+});
+//
+
+function totalInCart() {
+    const counts = JSON.parse(localStorage.getItem("counts")) || {};
+    console.log(counts);
+    const itemIds = Object.keys(counts);
+    console.log(itemIds);
+    let totalItems = 0;
+    itemIds.forEach(itemId => {
+    totalItems += counts[itemId];
+});
+  
+return totalItems;
+}
+const totalItems = totalInCart();
+console.log("isviso", totalItems);
+
+kainaNav.innerHTML= `
+<i class="fa-solid fa-cart-shopping"></i>Cart(${totalItems})
+`;
